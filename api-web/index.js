e153ac32f6291a27909442037e8cffcd2c9dd0dd
Luigi <luigiaubrypouget@gmail.com>
2023-05-12 14:46:38 +0200

    nodemon installé en script start

--- api-web/index.js
+++ api-web/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const path = require('path');
+
 const app = express();
 const port = process.env.port;
 
@@ -9,5 +10,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Listening to port ${port}.`)
+  console.log(`Listening to port ${port}.`);
 })
